b620d6ee770d56fc03404fe84f24802051372e5f
Catalin <catalin.sandru88@gmail.com>
2021-10-16 17:21:02 +0300

    scrape one website to return array of titles and links

--- index.js
+++ index.js
@@ -5,8 +5,26 @@ const cheerio = require('cheerio');
 const app = express();
 const port = 5000;
 
+const articles = []
+
 app.get('/', (req, res, next) => {
   res.json('welcome')
+})
+
+app.get('/news', (req, res) => {
+  axios.get('https://www.theguardian.com/environment/climate-crisis')
+  .then(response => {
+    const html = response.data
+    const $ = cheerio.load(html)
+    
+    $('a:contains("climate")', html).each(function() {
+      const title = $(this).text()
+      const url = $(this).attr('href')
+      articles.push({title, url})
+    })
+    res.json(articles)
+  })
+  .catch(err => console.log(err))
 })
 
 app.listen(port, () => {
